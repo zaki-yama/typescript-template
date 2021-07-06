59789b359214bc365514327452a756b1de22f98f
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-07-06 17:28:04 +0900

    chore(build): fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-
-  devtool: "cheap-module-eval-source-map",
+module.exports = (_, argv) => ({
   entry: "./src/index.tsx",
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist")
+    contentBase: path.join(__dirname, "dist"),
   },
+  devtool: argv.mode === "production" ? false : "inline-source-map",
   module: {
     rules: [
       {
+        exclude: /node_modules/,
         test: /\.tsx?$/,
-        use: "ts-loader"
-      }
-    ]
+        use: "ts-loader",
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  }
-};
+    extensions: [".ts", ".tsx", ".js"],
+  },
+});
